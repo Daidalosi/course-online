04f13f980e050c2f1502196339c921236f9acfe8
daidalosi <daidalosi@163.com>
2021-06-09 23:30:07 +0800

    增加子模块Eureka

---
+++ eureka/src/main/java/com/pinjhu/EurekaApplication.java
@@ -0,0 +1,10 @@
+package com.pinjhu;
+
+import org.springframework.boot.SpringApplication;
+
+public class EurekaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaApplication.class, args);
+  }
+}
